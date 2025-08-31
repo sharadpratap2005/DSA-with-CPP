d64bc322904f86fb97135df5af62d83c428370e0
Sharad Pratap <164340876+sharadpratap2005@users.noreply.github.com>
2025-08-31 16:28:05 +0530

    POTD | GFG | 31 Aug 2025 | Maximum Water Container

---
+++ GeeksforGeeks_Problems/Container_With_MAX_Water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        int n=arr.size();
+        int left=0;
+        int right=n-1;
+        int max_area=0;
+        while(left<right){
+            int height=min(arr[left],arr[right]);
+            int width=right-left;
+            int temp_area=height*width;
+                max_area=max(temp_area,max_area);
+            if(arr[left]<arr[right]){
+                left++;
+            }
+            else {
+                            right--;
+        }
+        
+        }
+        return max_area;
+        
+    }
+};
