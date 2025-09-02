650ba2270c35e9982c3a6b26129f38c24fdb0611
Sharad Pratap <164340876+sharadpratap2005@users.noreply.github.com>
2025-09-02 19:53:45 +0530

    2 SEP 2025

---
+++ GeeksforGeeks_Problems/Swap_Kth_Node.cpp
@@ -0,0 +1,73 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* swapKth(Node* head, int k) {
+        if(!head) return head;
+        
+        //find the length
+        int n = 0;
+        Node* temp=head;
+        while(temp){
+            n++;
+            temp=temp->next;
+        }
+        
+        //if k is greater than n
+        if(k>n) return head;
+        //if kth node from begining and kth from ending are same
+        if(2*k-1==n)return head;
+        
+        
+        //find the kth node and its previous node from the begining
+        Node* x_prev=NULL;
+        Node* x=head;
+        for(int i=1;i<k;i++){
+            x_prev=x;
+            x=x->next;
+        }
+        
+        //find the kth node from the end of linked list
+        Node* y_prev=NULL;
+        Node* y=head;
+        for(int i=1;i<n-k+1;i++){
+            y_prev=y;
+            y=y->next;
+        }
+        
+        //if x is not head
+        if(x_prev){
+            x_prev->next=y;
+        }
+        else{
+            head=y;
+        }
+        
+        //if y is not head
+        if(y_prev){
+            y_prev->next=x;
+        }
+        else{
+            head=x;
+        }
+        
+        //swap next pointers
+        Node* tempNext=x->next;
+        x->next=y->next;
+        y->next=tempNext;
+        
+        return head;
+
+    }
+};
