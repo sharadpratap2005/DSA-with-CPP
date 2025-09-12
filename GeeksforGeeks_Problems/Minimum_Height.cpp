1ff2ec00c5dc12cff762aa52ba98c1c6ee6f59e7
Sharad Pratap <164340876+sharadpratap2005@users.noreply.github.com>
2025-09-12 16:26:32 +0530

    POTD | GFG | 03 Sep 2025 | Minimum Height II

---
+++ GeeksforGeeks_Problems/Minimum_Height.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        int n=arr.size();
+        if(n==0)return 0;
+        sort(arr.begin(),arr.end());
+        int ans = arr[n-1]-arr[0];//intitial differnence
+        int smallest = arr[0]+k;
+        int largest = arr[n-1]-k;
+        
+        for(int i=0;i<n-1;i++){
+            int min_height=min(smallest,arr[i+1]-k);
+            int max_height=max(largest,arr[i]+k);
+            if(min_height<0)continue;
+            ans=min(ans,max_height-min_height);
+}
+        return ans;
+    }
+};
