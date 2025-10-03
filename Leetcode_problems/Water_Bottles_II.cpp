377be48238ac9972aa5fffb2212d72d3bc4294bc
Sharad Pratap <164340876+sharadpratap2005@users.noreply.github.com>
2025-10-03 09:47:03 +0530

    Update Water_Bottles_II.cpp

--- Leetcode_problems/Water_Bottles_II.cpp
+++ Leetcode_problems/Water_Bottles_II.cpp
@@ -11,3 +11,18 @@ public:
         return numBottles + count;
     }
 };
+//simple simulation 
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int drank = numBottles;
+        int empty = numBottles;
+        while (empty >= numExchange) {
+            empty -= numExchange;
+            drank++;
+            empty++;
+            numExchange++;
+        }
+        return drank;
+    }
+};
