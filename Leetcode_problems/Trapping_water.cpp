a8dfb09b84ab294feabc91679b73cb7c2ca3760f
Sharad Pratap <164340876+sharadpratap2005@users.noreply.github.com>
2025-10-03 21:30:37 +0530

    Update Trapping_water.cpp

--- Leetcode_problems/Trapping_water.cpp
+++ Leetcode_problems/Trapping_water.cpp
@@ -32,3 +32,35 @@ public:
         return ans;
     }
 };
+
+//Optimal Approach --> Time Complexity - O(N) & Space Complexity - O(1)
+// Use two pointers approach first we find the minimum of height[left] and height[right] using if else. Then we check if the height[left] or height[right] is greater than the leftMax and rightMax respectively then assign them the maxium values else add the value to the answer by leftMax - height[left] or rightMax - height[right].
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int ans = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    ans += leftMax - height[left];
+                left++;
+
+            } else {
+                if (height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    ans += rightMax - height[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+};
