b240d3a2d4efad867a0a88cba1d674434524472d
Sharad Pratap <164340876+sharadpratap2005@users.noreply.github.com>
2025-12-10 22:37:39 +0530

    POTD | LeetCode | 10 Dec 2025

---
+++ Leetcode_problems/Count_Computer_Permutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int M = 1e9 + 7;
+    long long factorial(int num) {
+        long long f = 1;
+        for (int i = 1; i <= num; i++) {
+            f = (f * i) % M;
+        }
+        return f;
+    }
+    int countPermutations(vector<int>& complexity) {
+        int n = complexity.size();
+        int ans = 0;
+        int mini = *min_element(complexity.begin(), complexity.end());
+        if (complexity[0] > mini)
+            return 0;
+        int count = 0;
+        for (int val : complexity) {
+            if (val == mini)
+                count++;
+            if (count == 2)
+                return 0;
+        }
+        ans = factorial(n - 1);
+        return ans;
+    }
+};
